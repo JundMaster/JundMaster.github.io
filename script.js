1d8d36cccd73f888eb42542657a333b989d87720
Glitch (glitch-blank-website) <none>
2022-12-06 17:46:30 +0000

    🎺👣 Checkpoint ./package.json:51803382/4977 ./script.js:51803382/57 ./index.html:51803382/213

--- script.js
+++ script.js
@@ -7,21 +7,21 @@ var pageId;
 
 const randomVar = "random name";
 
-(async () => {
-  const databaseId = '374afc0e7ad44137856fd4875113e817';
+// (async () => {
+//   const databaseId = '374afc0e7ad44137856fd4875113e817';
 
-  // const page = await notion.pages.retrieve({ page_id: "7c5b63ffb9444e4880842d4582f1b1d9", property_id: "title" });
-  // const dataBase = await notion.databases.retrieve({ database_id: databaseId });
+//   // const page = await notion.pages.retrieve({ page_id: "7c5b63ffb9444e4880842d4582f1b1d9", property_id: "title" });
+//   // const dataBase = await notion.databases.retrieve({ database_id: databaseId });
   
-  const {results} = await notion.databases.query({
-    database_id: databaseId,
-    filter: {
-      property: "Parent Location",
-      select: {
-        equals: "Silverymoon"
-      }
-    }
-  });
+//   const {results} = await notion.databases.query({
+//     database_id: databaseId,
+//     filter: {
+//       property: "Parent Location",
+//       select: {
+//         equals: "Silverymoon"
+//       }
+//     }
+//   });
   
 //   const collectionPagesIDs = results.map(page => page.id);
 //   var silverymoonPoints;
@@ -46,11 +46,11 @@ const randomVar = "random name";
   
   
   
-  const highPalacePage = (await notion.blocks.children.list({
-    block_id: "7c5b63ffb9444e4880842d4582f1b1d9",
-  })).results;
+//   const highPalacePage = (await notion.blocks.children.list({
+//     block_id: "7c5b63ffb9444e4880842d4582f1b1d9",
+//   })).results;
 
-  const test = highPalacePage.map((page) => page.paragraph.rich_text[0].text.content);
+//   const test = highPalacePage.map((page) => page.paragraph.rich_text[0].text.content);
   
-  console.log(test);
-})();
+//   console.log(test);
+// })();
