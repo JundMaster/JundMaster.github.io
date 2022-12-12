3bfb347fef8e7582ad625f69bb2d4169eaba827f
Glitch (glitch-blank-website) <none>
2022-12-12 10:31:57 +0000

    clean script a bit

--- script.js
+++ script.js
@@ -1,31 +1,11 @@
-
-var randomVar = {name: "random name"};
-var Pudim = {
-  name : "ABC",
-  age : 18,
-  dept : "CSE",
-  score : 90
-};
-
-var test;
-
-var places = [];
-
-
-
 const { Client } = require("@notionhq/client");
 
 const notion = new Client({
   auth: "secret_G6y1oOHPhQT1yBz6njJkjeziExKcPoHCXZvcqZ0C0vN"
 })
-var pageId;
-
 
 const getLocations = async () => {
   const databaseId = '374afc0e7ad44137856fd4875113e817';
-
-  // const page = await notion.pages.retrieve({ page_id: "7c5b63ffb9444e4880842d4582f1b1d9", property_id: "title" });
-  // const dataBase = await notion.databases.retrieve({ database_id: databaseId });
   
   // Filtered database that only contains Silverymoon locations
   const {results} = await notion.databases.query({
@@ -44,7 +24,6 @@ const getLocations = async () => {
   // Collection of the Location names
   const collectionLocationNames = results.map(page => page.properties.Name.title[0].text.content);
   
-  // console.log(collectionLocationNames);
   var silverymoonPoints;
 
   var silverymoonPoints = [];
@@ -76,47 +55,13 @@ const getLocations = async () => {
       location_info: tempoLocationInfo,
       iteration: i
     })
-      // silverymoonPoints.push({location_name: collectionLocationNames[i]});
-      // silverymoonPoints.push(tempBlock.map((block) => {
-      //   // console.log(block)
-      //   var tempoLocationInfo;
-      //   var currentIteration = i;
-      //   if (block.type == "paragraph")
-      //     tempoLocationInfo = block.paragraph.rich_text[0].plain_text
-      //   else
-      //     tempoLocationInfo = "not_paragraph";
-      
-        
-      //   return{
-      //     location_name: collectionLocationNames[i],
-      //     location_info: tempoLocationInfo,
-      //     iteration: i
-      //     // location_info: block,
-      //     // location_info: block.paragraph.rich_text[0].plain_text,
-      //   }
-      // }))  
-      // test.push(tempPage.map((page) => page.paragraph.rich_text[0].plain_text))  
   }
 
-  // console.log(silverymoonPoints[0].location_info[0].info_type);
   return silverymoonPoints;
-  
-  
-  
-  
-  // const highPalacePage = (await notion.blocks.children.list({
-  //   block_id: "7c5b63ffb9444e4880842d4582f1b1d9",
-  // })).results;
-
-  // test = highPalacePage.map((page) => page);
-  // console.log(test);  
-
 };
 
-// getLocations();
 (async () => {
   const nLocations = await getLocations()
-  // console.log(nLocations)
 })()
 
 const express = require('express');
@@ -180,5 +125,3 @@ function onListening() {
     : 'port ' + addr.PORT;
   debug('Listening on ' + bind);
 }
-
-module.exports = {randomVar, Pudim, test};
